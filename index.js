c98d77a22704e5f12bff718b90584fb27923700d
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-12 19:26:46 +0600

    delete btn fixed in dashboard

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v73g3gy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,11 +40,37 @@ async function run() {
             res.send(result);
         });
 
+
+
         app.post('/selectedClass', async (req, res) => {
             const selectedClass = req.body;
+            const query = { classId: selectedClass.classId } //cheak dublicate
+            const isExists = await selectedClasses.findOne(query)
+            if (isExists) {
+                return res.send({ message: 'All Ready Exists' })
+            }
             const result = await selectedClasses.insertOne(selectedClass);
             res.send(result);
         });
+
+        app.get('/selectedClass', async (req, res) => {
+            const result = await selectedClasses.find().toArray();
+            res.send(result);
+        });
+
+
+        app.delete('/selectedClass/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await selectedClasses.deleteOne(query)
+            res.send(result);
+        });
+
+
+
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
