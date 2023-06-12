dd3dc701def9b8e8dd0d6d2226a9b6bd107bfcb2
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-12 15:47:04 +0600

    added selected data

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const app = express()
+const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
 // middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // mongodb connection
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v73g3gy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,18 +28,23 @@ async function run() {
 
         const classesCollection = client.db("lotusGrove").collection("classes");
         const extraClassCollection = client.db("lotusGrove").collection("extrasection");
-
+        const selectedClasses = client.db("lotusGrove").collection("selected_classes");
 
         app.get('/classes', async (req, res) => {
-            const result = await classesCollection.find().toArray()
-            res.send(result)
-        })
+            const result = await classesCollection.find().toArray();
+            res.send(result);
+        });
 
         app.get('/extra', async (req, res) => {
-            const result = await extraClassCollection.find().toArray()
-            res.send(result)
-        })
+            const result = await extraClassCollection.find().toArray();
+            res.send(result);
+        });
 
+        app.post('/selectedClass', async (req, res) => {
+            const selectedClass = req.body;
+            const result = await selectedClasses.insertOne(selectedClass);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -51,14 +54,13 @@ async function run() {
         // await client.close();
     }
 }
+
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('boss is sitting')
-})
+    res.send('boss is sitting');
+});
 
 app.listen(port, () => {
-    console.log(`Lotusgrove is running ${port}`)
-})
+    console.log(`Lotusgrove is running ${port}`);
+});
