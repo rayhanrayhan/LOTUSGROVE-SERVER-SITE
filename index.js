c592b16a77919e9e401b43a4b818aaa4e8b7e52a
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-14 15:01:40 +0600

    await client commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const classesCollection = client.db("lotusGrove").collection("classes");
         const extraClassCollection = client.db("lotusGrove").collection("extrasection");
