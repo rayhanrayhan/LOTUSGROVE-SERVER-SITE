5d57120e64d883aa920f4b19c85d1fcf2b86f575
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-18 23:31:05 +0600

    fixed some problem

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.send.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    //bearer token
+    const token = authorization.split('')[1];
+
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (er, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+
+
+
+
 // mongodb connection
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v73g3gy.mongodb.net/?retryWrites=true&w=majority`;
@@ -101,8 +126,16 @@ async function run() {
 
 
         // get this data clint site releted post method
-        app.get('/selectedClass/:email', async (req, res) => {
+        app.get('/selectedClass/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
+            if (!email) {
+                res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
             const query = { email: email }
             const result = await selectedClasses.find(query).toArray();
             res.send(result);
